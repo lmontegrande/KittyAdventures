a09a500ac0e4c20e085afdcf810e12f05282de41
Laverii <phoenixpyre_xx@yahoo.com>
2017-11-01 22:12:56 -0700

    Added pick up script and soul shard sprite

---
+++ Assets/Scripts/pickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour {
+
+    public int soulAmnt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(other);
+            soulAmnt++;
+        }
+    }
+}
