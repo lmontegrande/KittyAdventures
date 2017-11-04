473777fe2c26694ae59aac42f5ff06af4fb8d592
Laverii <phoenixpyre_xx@yahoo.com>
2017-11-04 00:16:33 -0700

    added door stuff, still wip

---
+++ Assets/Scripts/doorSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            this.GetComponent<BoxCollider2D>.enabled = false;
+        }
+    }
+}
