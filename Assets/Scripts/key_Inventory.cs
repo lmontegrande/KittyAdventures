3a52ae9b8587d814dce57ca18884f247f69df40f
Laverii <phoenixpyre_xx@yahoo.com>
2017-11-12 22:33:10 -0800

    Updated UI and chest/soul shard prefab to workk with UI.

--- Assets/Scripts/key_Inventory.cs
+++ Assets/Scripts/key_Inventory.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class key_Inventory : MonoBehaviour {
 
     public bool keyInv;
+
+    public Image gotKey;
 	// Use this for initialization
 	void Start () {
-		
+        gotKey.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -19,6 +21,7 @@ public class key_Inventory : MonoBehaviour {
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            gotKey.enabled = true;
             Destroy(GetComponent<CircleCollider2D>());
             GetComponent<SpriteRenderer>().sprite = null;
             keyInv = true;
